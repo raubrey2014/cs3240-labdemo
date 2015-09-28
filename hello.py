18545fee4c4eb7afd890613228f1e1a30b2ba063
Ryan Aubrey <rma7qb@virginia.edu>
2015-09-28 17:05:08 -0400

    Committing helper version of the implementation

--- hello.py
+++ hello.py
@@ -1,6 +1,3 @@
-def greeting(msg):
-	print(msg)
-
-
+import helper
 if __name__ == "__main__":
-	greeting('Hello')	
+	helper.greeting('Hello')	
---
+++ helper.py
@@ -0,0 +1,6 @@
+def greeting(msg):
+	print(msg)
+
+
+if __name__ == "__main__":
+	greeting('Hello')	
